5c0a63aba51a9321d3f5f16cfc8ca73897c1dce3
cosa65 <mfosco65@gmail.com>
2017-10-03 15:32:56 -0300

    normalization

--- TP2/bins_scott.cpp
+++ TP2/bins_scott.cpp
@@ -1,5 +1,27 @@
+##### 2.1 e) #####
 
-def bins_scott(electrode):
-    diff = max(electrode) - min(electrode)
-    div = 3.5*np.std(electrode)*len(electrode)**(-1/3)
-    return diff/div
+def normalized(list):
+    sum = sum(list)
+    return list.map(lambda x: x/sum)
+
+##b)##
+import numpy as np
+
+bandas = [0, 4, 8, 13, 30, 45]
+bandaspaciente = []
+
+for paciente in pacientes:
+    dtfspatient = None
+    test = sio.loadmat(paciente)["data"]
+    avg1 = np.mean(test, axis = 1)
+    avg2 = np.mean(avg1, axis = 0)
+    freq, pot = sig.welch(avg2, fs = 250, nfft = 2048)
+    bandaspaciente.append([])
+    
+    for i in range(len(bandas) -1):
+        inicio = bandas[i] 
+        fin = bandas[i+1]
+        valores = [pot[i] for i in range(len(pot)) if freq[i] >= inicio and freq[i] < fin]
+        bandaspaciente[-1].append(sum(valores))
+        
+pd.DataFrame(normalized(bandaspaciente), columns = ["delta", "theta", "alpha", "beta", "gamma"])
